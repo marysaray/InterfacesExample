01cdd6126952ce71711fb7c691de2f638c317ed0
Mary Saray <msaray.ms@outlook.com>
2021-09-01 10:24:41 -0700

    Created IDisplayable interface

---
+++ InterfacesExample/IDisplayable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample
+{
+    interface IDisplayable // all interfaces are public by default
+    {
+        string GetDisplayText();
+    }
+}
