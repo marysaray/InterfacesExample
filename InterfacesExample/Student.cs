1e2d93c111eddae8d744da10ac943a033f087056
Mary Saray <msaray.ms@outlook.com>
2021-09-01 11:05:40 -0700

    Added IComparable<T> Interface for student class to be compared alphabetically

--- InterfacesExample/Student.cs
+++ InterfacesExample/Student.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace InterfacesExample
 {
-    class Student : IDisplayable
+    class Student : IDisplayable, IComparable<Student>
     {
         public string StudentId { get; set; }
         
         public string FullName { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            #region // compare students logic
+            /*
+            if (this.FullName < other.FullName)
+            {
+                return -1;
+            }
+            else if (this.FullName == other.FullName)
+            {
+                return 0;
+            }
+            else 
+            {
+                return 1;
+            }
+            */
+            #endregion
+            return this.FullName.CompareTo(other.FullName); // all built-in types has a compareTo method 
+        }
 
         public string GetDisplayText()
         {
