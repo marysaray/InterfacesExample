0d73fcc06dea7aa0f3277e896b792727dda44d7b
Mary Saray <msaray.ms@outlook.com>
2021-09-01 10:14:49 -0700

    Created student class

---
+++ InterfacesExample/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExample
+{
+    class Student
+    {
+        public string StudentId { get; set; }
+        
+        public string FullName { get; set; }
+
+        public void Register(string courseCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
