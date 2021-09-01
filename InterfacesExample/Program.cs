b783dfbbf20690c07b25b5385699421cf724ab74
Mary Saray <msaray.ms@outlook.com>
2021-09-01 10:20:17 -0700

    Instantiated student and product objects

--- InterfacesExample/Program.cs
+++ InterfacesExample/Program.cs
@@ -10,6 +10,18 @@ namespace InterfacesExample
     {
         static void Main(string[] args)
         {
+            Student student = new Student()
+            {
+                StudentId = "789654123",
+                FullName = "Mary Saray"
+            };
+
+            Product product = new Product()
+            {
+                ProductId = 1,
+                Price = 10.00,
+                Title = "Student Id"
+            };
         }
     }
 }
